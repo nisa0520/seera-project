56135d9c08a1230bcf7e785e6bb348bfa46fc3db
Tim1357912 <timothy.elroy.tif23@polban.ac.id>
2026-04-13 09:00:00 +0700

    Tim1357912: feat(frontend): add initial chatbot UI and API integration

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
     <Navbar />
     <RouterView />
     <Footer />
+    <ChatbotWidget />
   </div>
 </template>
 
 <script setup>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+import ChatbotWidget from './components/chatbot/ChatbotWidget.vue'
 </script>
